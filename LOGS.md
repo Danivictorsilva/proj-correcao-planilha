75fcb108dd13a5d1d9e909a5af448accd718402e
Daniel Victor Santos Silva <danivictorsilva@hotmail.com>
2023-03-12 19:10:23 -0300

    read just one cell

---
+++ LOGS.md
@@ -0,0 +1,6 @@
+### 06/03/2023
+
+- Testar com meu arquivo 'PlanilhaIntegracaoNumerica'
+- Checar resultado final com uma margem
+- Testar planilha que corrige
+- Verificar proteção de células
--- src/index.php
+++ src/index.php
@@ -1,8 +1,3 @@
-<?php
-    use Shuchkin\SimpleXLSX;
-
-?>
-
 <!DOCTYPE html>
 <html>
 
@@ -16,76 +11,32 @@
     </form>
 
     <?php
+    use Shuchkin\SimpleXLSX;
 
-    function Avaliar($file)
-    {
-        $header = array(1 => "x1 (água)", 2 => "r (kg/m3)", 3 => "v (m3/kg)", 4 => "v (m3/mol) x 106", 5 => "Dvmis (m3/mol) x 106", 6 => "Modelo", 7 => "", 8 => "     parcial molar (m³/mol) x 106", 9 => "     parcial molar (m³/mol) x 106", 10 => "v (m3/mol) calculado x 106", 11 => "p (kg/m3) calculado");
-        $erros = array();
+    ini_set('error_reporting', E_ALL);
+    ini_set('display_errors', true);
 
-        $xlsx = new SimpleXLSX($file); // try...catch
-        if ($xlsx->success()) {
-            $lin = 0;
-            foreach ($xlsx->rows() as $r) {
-                $lin = $lin + 1;
-                $col = 0;
-                foreach ($r as $cell) {
-                    if ($lin == 2 && $col > 0 && $col < 12) {
-                        if ($header[$col] != $cell) {
-                            array_push($erros, "A ordem das colunas foi alterada ou a planilha foi modificada. Era esperado <strong>'" . $header[$col] . "'</strong> na posi&ccedil;&atilde;o <strong>" . chr($col + 65) . ($lin) . "</strong> ao inv&eacute;s de <strong>" . $cell . "</strong><br>");
-                        }
-                    }
-                    $col = $col + 1;
-                }
-            }
-        } else {
-            echo 'xlsx error: ' . $xlsx->error();
-        }
-        return $erros;
-    }
+    require_once __DIR__ . '/../vendor/autoload.php';
 
     if (isset($_FILES['fileToUpload'])) {
         if (
             $_FILES['fileToUpload']['error'] == UPLOAD_ERR_OK               //checks for errors
             && is_uploaded_file($_FILES['fileToUpload']['tmp_name'])
         ) { //checks that file is uploaded
-
-            $header = array(1 => "x1 (água)", 2 => "r (kg/m3)", 3 => "v (m3/kg)", 4 => "v (m3/mol) x 106", 5 => "Dvmis (m3/mol) x 106", 6 => "Modelo", 7 => "", 8 => "     parcial molar (m³/mol) x 106", 9 => "     parcial molar (m³/mol) x 106", 10 => "v (m3/mol) calculado x 106", 11 => "p (kg/m3) calculado");
-
+            
             $file = $_FILES['fileToUpload']['tmp_name'];
             echo "<p>Arquivo carregado com sucesso!</p>";
-            $xlsx = new SimpleXLSX($file); // try...catch
-            echo "<pre>";
+            $xlsx = new SimpleXLSX($file); 
             if ($xlsx->success()) {
-                $lin = 0;
-                foreach ($xlsx->rows() as $r) {
-                    $lin = $lin + 1;
-                    $col = 0;
-                    foreach ($r as $cell) {
-                        if ($lin == 2 && $col > 0 && $col < 12) {
-                            if ($header[$col] == $cell) {
-                                echo "*";
-                            }
-                            echo "*";
-                        }
-                        $col = $col + 1;
-                        echo ($cell);
-                        echo ",";
-                    }
-                    echo "<br>";
-                }
+                echo 'Celula I10 ='.$xlsx->getCell(0, 'I10');
+            
             } else {
-                echo 'xlsx error: ' . $xlsx->error();
+                echo 'xlsx error: '.$xlsx->error();
             }
-            echo "</pre>";
-            echo "<br>";
-            $erros = Avaliar($file);
-            foreach ($erros as $e) {
-                echo $e;
-            }
+            
         }
     }
     ?>
 
 </body>
-
 </html>
